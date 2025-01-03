51cf578c0aaf5af046b0d68d84f037a58c2ac7a8
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-03 21:12:32 +0530

    My first java program

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //write your code here.
+        System.out.println("Hello Kajal!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
