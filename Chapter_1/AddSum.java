36b891f7bf0ca5811d865fdefceba83a735367bf
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-07 15:21:45 +0530

    done

---
+++ Chapter_1/AddSum.java
@@ -0,0 +1,11 @@
+public class AddSum {
+    public static void main(String[] args) {
+        //write your code here.
+        System.out.println("The sum of these numbers is: ");
+        int num1 = 2;
+        int num2 = 10;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+         System.out.println(sum);  
+    }
+}
