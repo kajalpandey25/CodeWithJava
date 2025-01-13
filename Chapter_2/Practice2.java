dec48a2ac0ba4ef466f61bbd0e15c256db937f63
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-13 16:35:08 +0530

    Done

--- Chapter_2/Practice2.java
+++ Chapter_2/Practice2.java
@@ -1,6 +1,8 @@
 // Q.1 -> Write a program to sum three numbers in java.
 // Q.2 -> Write a program to calculate CGPA using marks of three subjects (out of 100)
+//  Q.3 -> Write a java program which asks a user to enter his/her name and greet them with "Hello <name> , have a good day" text.
 
+import java.util.Scanner;
 public class Practice2{
     public static void main(String[] args){
     //   Question 1
@@ -11,11 +13,18 @@ public class Practice2{
     //   System.out.println(sum);
 
     //   Question 2
-        int subject1 = 45;
-        int subject2 = 95;
-        int subject3 = 48;
-        int cgpa = (subject1 + subject2 + subject3)/30;
-        System.out.println(cgpa);
+        // float subject1 = 45;
+        // float subject2 = 95;
+        // float subject3 = 48;
+        // float cgpa = (subject1 + subject2 + subject3)/30;
+        // System.out.println(cgpa);
+
+        // Question 3
+
+        System.out.println("What is your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name + " have a good day!");
 
     }
 }      
