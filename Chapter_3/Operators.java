f93e9d1e2c61f4876c33c3be9887c55216b4d31d
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-17 22:45:46 +0530

    done

--- Chapter_3/Operators.java
+++ Chapter_3/Operators.java
@@ -1,13 +1,43 @@
 public class Operators {
     public static void main(String[] args){
-        int a = 4;
-        // int b = 6 % a; // modulo operator
-        int b = 9;
-        b += 5;
 
-        int c = 10;
-        c -= 5;
-        System.out.println(b);
-        System.out.println(c);
+        // 1. Arithmetic Operators
+
+        // int a = 20;
+        // int b = 10;
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println(a%b);
+
+        // Assignment Operators
+        // int b = 9;
+        // b += 5;
+        // b -= 5;
+        // b *= 2;
+    // System.out.println(b);
+
+      // 2. Comparison Operators
+
+    //   System.out.println(6==8); // false
+    //   System.out.println(6!=8); // true
+    //   System.out.println(64>6); // true
+    //   System.out.println(64<6); // false
+    //   System.out.println(20>=5); // true
+    //   System.out.println(20<=10); // false
+
+    // 3. Logical Operators
+
+    // System.out.println(64>50 && 64>40); // true
+    // System.out.println(64>10 || 64>98); // true
+
+    // 4. Bitwise Operators
+
+//     10
+//     11
+//   ----------------
+//     10  
+      System.out.println(2&3); // 2
     }
 }
