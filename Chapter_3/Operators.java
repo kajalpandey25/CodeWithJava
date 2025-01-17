8aadc386e6c5bf480085e9708b407c50ba0d2f7b
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-17 15:04:16 +0530

    Done

--- Chapter_3/Operators.java
+++ Chapter_3/Operators.java
@@ -1,3 +1,13 @@
+public class Operators {
+    public static void main(String[] args){
+        int a = 4;
+        // int b = 6 % a; // modulo operator
+        int b = 9;
+        b += 5;
 
-
-
+        int c = 10;
+        c -= 5;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
