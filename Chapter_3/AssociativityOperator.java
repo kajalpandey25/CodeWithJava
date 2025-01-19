3e8704c3ae0cc96937556f5f78b676dab2571d62
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-19 20:49:10 +0530

    done

--- Chapter_3/AssociativityOperator.java
+++ Chapter_3/AssociativityOperator.java
@@ -1,10 +1,34 @@
 public class AssociativityOperator{
     public static void main(String[] args){
+         // Precedence & Associativity
         int a =  6*5-34/2;
+        /*
+        Highest precedence goes to * and /. They are then evaluated on the basis of left to right associativity.
+        = 30-34/2
+        =30-17
+        =13
+        */
         int b = 60/5-34*2;
-        System.out.println(ba;
+        /*
+           = 12-34*2
+           = 12-68
+           = -54
+        */
+        System.out.println(a);
         System.out.println(b);
 
-        // Precedence & Associativity
+        // Quick Quiz
+
+         int x = 6;
+         int y = 1;
+        int k = x * y/2;
+
+        int x = 5;
+        int y = 1;
+        int z = 4;
+        int k = y*y - (4*x*z)/(2*x);
+         System.out.println(k);
+
+
     }
 }
