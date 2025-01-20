4e6258e22810986b7351e0cf93c90fb66907e0e2
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-20 20:35:04 +0530

    done

---
+++ Chapter_3/PracticeSet.java
@@ -0,0 +1,10 @@
+// Question:1 What will be  the result of the following expression
+// float a = 7/4*9/2
+
+
+public class PracticeSet{
+    public static void main(String[] args){
+        float a = 7/4 * 9/2;
+        System.out.println(a);
+    }
+}
