1462fb1a74d2a0e3ee9791371f2f07fba79a194d
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-20 15:39:51 +0530

    done

---
+++ Chapter_3/ResultingDataType.java
@@ -0,0 +1,30 @@
+public class ResultingDataType{
+    public static void main(String[] args){
+        // byte x = 5;
+        // int y = 6;
+        // short z = 8;
+        // int a = y+z;
+        // float b = 6.54f+x;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Increment and Decrement Operators
+        // int i = 56;
+        // int b = i++; // first b is assigned i then i is incremented(56)
+        // int j = 67;
+        // int c = ++j; // first j is incremented then c is assigned j(68)
+        // System.out.println(i++);
+        // System.out.println(i);
+        // System.out.println(++i);
+        // System.out.println(i);
+
+
+        // quick quiz : What will be the value of the following expression(x).
+             int y = 7;
+             int x = ++y+8;
+             System.out.println(x);
+
+             char ch = 'a';
+             System.out.println(++ch);
+    }
+}
