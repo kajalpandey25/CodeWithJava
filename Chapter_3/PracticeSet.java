ef8b498fb28471b5ea9a8d30008766d03500a5c7
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-22 21:16:21 +0530

    done

--- Chapter_3/PracticeSet.java
+++ Chapter_3/PracticeSet.java
@@ -1,21 +1,58 @@
 // Question:1 What will be  the result of the following expression
 // float a = 7/4*9/2
 
+import java.util.Scanner;
 
 public class PracticeSet{
     public static void main(String[] args){
-        float a = 7/4.0f * 9/2.0f;
-        System.out.println(a);
+        // float a = 7/4.0f * 9/2.0f;
+        // System.out.println(a);
 
 
 // Question:2 Write a java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade.
-       char grade = '8';
-       grade = (char)(grade + 8);
-       System.out.println(grade);
+    //    char grade = '8';
+    //    grade = (char)(grade + 8);
+    //    System.out.println(grade);
 
-    //    Decrypting the grade
-    grade = (char) (grade - 8);
-    System.out.println(grade);
+    // //    Decrypting the grade
+    // grade = (char) (grade - 8);
+    // System.out.println(grade);
+
+
+    // Question:3 Use comparison operators to find out, whethere a given number is greater than tha user enterted number or not.
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // System.out.println(a>8);
+
+
+        // Question:4 Write the following expression in a java program:
+        //     v^2 - u^2
+        //   ---------------
+        //        2as
+
+         // Declare variables
+        double v = 20; // Final velocity
+        double u = 10; // Initial velocity
+        double a = 5;  // Acceleration
+        double s = 15; // Displacement
+
+        // Calculate the expression
+        double result = (Math.pow(v, 2) - Math.pow(u, 2)) / (2 * a * s);
+
+        // Print the result
+        System.out.println("The result of the expression is: " + result);
+
+
+
+
+// Question:5  Find the value of the following expression:
+//        int x = 7;
+//        int a = 7*49/7+35/7
+// value of a?
+                 
+        //  int x=7;
+        //  int a = 7*49/7+35/7;
+        //  System.out.println(a);
     
     }
 }
