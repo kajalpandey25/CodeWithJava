88a237a6e7d74c85f230c6b08d85be48f4c7701c
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-22 21:17:07 +0530

    done

--- Chapter_3/PracticeSet.java
+++ Chapter_3/PracticeSet.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 
 public class PracticeSet{
     public static void main(String[] args){
-        // float a = 7/4.0f * 9/2.0f;
-        // System.out.println(a);
+        float a = 7/4.0f * 9/2.0f;
+        System.out.println(a);
 
 
 // Question:2 Write a java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade.
-    //    char grade = '8';
-    //    grade = (char)(grade + 8);
-    //    System.out.println(grade);
+       char grade = '8';
+       grade = (char)(grade + 8);
+       System.out.println(grade);
 
-    // //    Decrypting the grade
-    // grade = (char) (grade - 8);
-    // System.out.println(grade);
+    //    Decrypting the grade
+    grade = (char) (grade - 8);
+    System.out.println(grade);
 
 
     // Question:3 Use comparison operators to find out, whethere a given number is greater than tha user enterted number or not.
-        // Scanner sc = new Scanner(System.in);
-        // int a = sc.nextInt();
-        // System.out.println(a>8);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);
 
 
         // Question:4 Write the following expression in a java program:
@@ -50,9 +50,9 @@ public class PracticeSet{
 //        int a = 7*49/7+35/7
 // value of a?
                  
-        //  int x=7;
-        //  int a = 7*49/7+35/7;
-        //  System.out.println(a);
+         int x=7;
+         int a = 7*49/7+35/7;
+         System.out.println(a);
     
     }
 }
