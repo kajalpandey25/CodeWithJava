c08e31061c59b818f96d4bcfc5c4db3f9631b4a1
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-23 16:16:16 +0530

    done

---
+++ Chapter_4/Strings.java
@@ -0,0 +1,12 @@
+public class Strings{
+    public static void main(String[] args){
+        // String name = new String("Kajal");
+        // String name = "Kajal";
+        // System.out.println("The name is: ");
+        // System.out.println(name);
+
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("The value of a is %d and value of b is %f", a,b);
+    }
+}
