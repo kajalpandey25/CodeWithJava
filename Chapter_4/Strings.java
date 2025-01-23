949cb3da93ccb232ce92ba660b788ba0cf50378d
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-01-23 21:28:22 +0530

    done

--- Chapter_4/Strings.java
+++ Chapter_4/Strings.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Strings{
     public static void main(String[] args){
         // String name = new String("Kajal");
         // String name = "Kajal";
-        // System.out.println("The name is: ");
-        // System.out.println(name);
+        System.out.println("The name is: ");
+        System.out.println(name);
 
         int a = 6;
         float b = 5.6454f;
         System.out.printf("The value of a is %d and value of b is %f", a,b);
+
+        Scanner sc = new Scanner(System.in);
+        // String st = sc.next(); // only take the first word
+        String st = sc.nextLine(); 
+        System.out.println(st);
     }
 }
